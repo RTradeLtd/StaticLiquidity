da8cfc5fa266615a772eaf85f4c89cdf5fad5ef7
potsables <postables@rtradetechnologies.com>
2018-12-19 00:37:07 -0800

    final commit

--- README.md
+++ README.md
@@ -6,4 +6,30 @@ This contract enables static liquidity for a token, allowing users to send token
 
 When setting the exchange rate, liquidity must be disabled. This is done to prevent abuse  by the contract operator in which a user sends a a transaction to purchase tokens at price X, but before the transaction is mined the contract operator sets the price to X+Y, resulting in the user receiving less tokens than they expected, but the contract operator receives the same amount of ether for less tokens.
 
-This should work with any token decimals, however it is best to use a token with 18 decimals.
+## Selling Tokens
+
+1) Approve the StaticLiquidity contract to transfer tokens on your behalf
+2) Invoke the `sellTokens` function, specifying the amount of tokens to sell in units of wei
+
+## Setup Instructions
+
+1) Deploy contract specifying the token contract to receive tokens in exchange for eth
+2) Set exchange rate (this is the amount of wei to be sent in exchange for 1.0 tokens)
+3) Deposit X amount of ethereum
+4) Enable Liquidity
+
+## Change Exchange Rate
+
+1) Disable Liquidity
+2) Set exchange rate (this is the amount of wei to be sent in en exchange for 1.0 tokens)
+3) Enable liquidity
+4) call `setExchangeRate`
+
+## Withdrawing Ether
+
+1) Disable Liquidity
+2) call `withdrawEther`
+
+## Topping Up Ethereum Balance
+
+To top up the available ethereum balance, send whatever amount of ethereum you want to the contract. Liquidity doesn't need to be disabled to top up the ethereum balance. Liquidity only needs to be disabled whenever you want to change the exchange rate.
--- StaticLiquidity.sol
+++ StaticLiquidity.sol
@@ -39,6 +39,7 @@ contract StaticLiquidity {
 
     /**
         * @notice Constructor function used to initialize storage variables during contract deployment
+        * @dev Once set, the token address can't be reconfigured.
         * @param _tokenContractAddress This is the address of the token this contract will accept in exchange for eth
      */
     constructor(address _tokenContractAddress) public {
@@ -56,6 +57,7 @@ contract StaticLiquidity {
 
     /**
         * @notice Used to enable liquidity, allowing selling of tokens for eth
+        * @dev Called must be admin
         * @return boolean, used to indicate that the operation was successful
      */
     function enableLiquidity() public isAdmin returns (bool) {
@@ -65,6 +67,7 @@ contract StaticLiquidity {
 
     /**
         * @notice Used to disable liquidity, preventing the selling of tokens for eth
+        * @dev Caller must be admin
         * @return boolean, use to indicate that the operation was successful
      */
     function disableLiquidity() public isAdmin returns (bool) {
@@ -74,7 +77,7 @@ contract StaticLiquidity {
 
     /**
         * @notice Used to set the exchange rate of wei per tokens
-        * @dev Caller must be administrator, and liquidity must be disabled
+        * @dev Caller must be admin, and liquidity must be disabled
         * @param _weiPerToken This is the amount of wei that will be sent in exchange for a single (1.0) token
         * @return boolean, used to indicate that the operation was successful
      */
@@ -85,13 +88,23 @@ contract StaticLiquidity {
 
     /**
         * @notice Used to withdraw any remaining tokens that are in the contract
-        * @dev Liquidity must be disabled first
+        * @dev Caller must be admin
         * @return boolean, use to indicate that the operation was successful
      */
-    function withdrawnTokens() public isAdmin isNotLiquid returns (bool) {
+    function withdrawnTokens() public isAdmin returns (bool) {
         uint256 contractTokenBalance = erc20I.balanceOf(address(this));
         require(contractTokenBalance > 0, "contract must have a token balance greater than 0");
         require(erc20I.transfer(msg.sender, contractTokenBalance), "token transfer failed");
+        return true;
+    }
+
+    /**
+        * @notice Used to withdraw any remaining ether in the contract
+        * @dev Caller must be admin, and liquidity must be disabled
+        * @return boolean, used to indicate that the operation was successful
+     */
+    function withdrawEther() public isAdmin isNotLiquid returns (bool) {
+        msg.sender.transfer(address(this).balance);
         return true;
     }
 
@@ -101,7 +114,8 @@ contract StaticLiquidity {
 
     /**
         * @notice Used to sell tokens in exchange for eth at a fixed price
-        * @param _tokensToSell This is the number of tokens to sell
+        * @dev Contract must be liquid 
+        * @param _tokensToSell This is the number of tokens to sell in units of wei
      */
     function sellTokens(uint256 _tokensToSell) public isLiquid returns (bool) {
         // send tokens to the contract
